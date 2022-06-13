54478b36a4d2980966954057986a20f21e31770e
Damien Bonnet <bonnet.damien@ymail.com>
2022-06-13 10:21:21 +0200

    [:clown_face: MOCK]  test api fetch
    Closes #3

--- src/components/templates/dashboard.tsx
+++ src/components/templates/dashboard.tsx
@@ -1,9 +1,29 @@
+import { usersService } from '@/api/users.service';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { useState } from 'react';
 import { Layout } from '../layout';
 
 export const DashboardTemplate = () => {
+  const [user, setUser] = useState<any | undefined>(undefined);
+
+  const fetchUser = useCallback(async () => {
+    try {
+      const data = await usersService.getById();
+
+      setUser(data);
+    } catch (e) {
+      console.error('FetchUser: ', e);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
     <Layout>
-      <h1>Dashboard template</h1>
+      <pre>{JSON.stringify(user)}</pre>
     </Layout>
   );
 };
